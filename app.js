aca79104194ed7a408303d8e3f9695c5ef0d8c56
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-04-15 15:16:39 -0400

    testing

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ app.use( bodyParser.json());
 
 app.get('/webhook/', function(req,res){
 	if(req.query['hub.verify_token'] === 'CAAMO2Q6yOFgBAG2io6VBdbplg8cCmF5ZC2rydqNACYmBZCIKKa8JZAmHVPISsCXSKhWrLMdHNMRWdUEZC7h5HEGtkEPKcuVe7rPuFyWwlwhOrSdZAGMegSh6dyW7G3h4sY5Ptfr8y762QFXp08pQOOfiFCIXcWLvB0NvWFSH416ZAPJqv7hUmIO5b8mhrZBDurxkaKZBONRQCQZDZD'){
-	    
+
 	    res.send(req.query['hub.challenge']);
 
-	}
+	}else{
 
-	res.send('Error, wrong validation token');
+		res.send('Error, wrong validation token');
+
+	}
     });
 
 app.listen(port, function() {
-	
+
 	console.log("listening");
 
     });
