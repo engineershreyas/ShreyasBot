7a8946a4df4009eb231f56bc31596e69692026c9
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-04-15 22:25:07 -0400

    echp

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use( bodyParser.json());
 
-app.post('/webhook/', function(req,res){
+var token = 'CAAMO2Q6yOFgBAG2io6VBdbplg8cCmF5ZC2rydqNACYmBZCIKKa8JZAmHVPISsCXSKhWrLMdHNMRWdUEZC7h5HEGtkEPKcuVe7rPuFyWwlwhOrSdZAGMegSh6dyW7G3h4sY5Ptfr8y762QFXp08pQOOfiFCIXcWLvB0NvWFSH416ZAPJqv7hUmIO5b8mhrZBDurxkaKZBONRQCQZDZD';
+
+app.get('/webhook/', function(req,res){
 	if(req.query['hub.verify_token'] === 'CAAMO2Q6yOFgBAG2io6VBdbplg8cCmF5ZC2rydqNACYmBZCIKKa8JZAmHVPISsCXSKhWrLMdHNMRWdUEZC7h5HEGtkEPKcuVe7rPuFyWwlwhOrSdZAGMegSh6dyW7G3h4sY5Ptfr8y762QFXp08pQOOfiFCIXcWLvB0NvWFSH416ZAPJqv7hUmIO5b8mhrZBDurxkaKZBONRQCQZDZD'){
 
 	    res.send(req.query['hub.challenge']);
@@ -19,7 +21,61 @@ app.post('/webhook/', function(req,res){
 		res.send('Error, wrong validation token');
 
 	}
-    });
+});
+
+app.post('/webhook/', function(req,res){
+
+var messaging_events = req.body.entry[0].messaging;
+
+for(var i = 0; i < messaging_events.length; i++) {
+
+	var event = req.body.entry[0].messaging[i];
+	var sender = event.sender.id;
+
+	if(event.message && event.message.text) {
+
+			text = event.message.text;
+			sentTextMessage(sender,text);
+
+	}
+
+}
+
+});
+
+function sentTextMessage(sender, text) {
+
+	messageData = {
+		text:text
+	}
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	},	function(error, response, body) {
+
+		if(error) {
+
+			console.log('Error sending message: ',error);
+
+		} else if(response.body.error) {
+
+			console.log('Error: ', response.body.error);
+
+		}
+
+	});
+
+
+)
+
+
+}
 
 app.listen(port, function() {
 
