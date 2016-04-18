d1da5584393087d5773f8f7b8adb6ed9a16b2274
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-04-18 13:53:55 -0400

    debugging

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.get('/webhook/', function(req,res){
 
 app.post('/webhook/', function(req,res){
 
+	console.log("message received\n");
+
 	messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
